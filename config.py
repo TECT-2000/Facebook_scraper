867322c61a42740bca8a0052a6b39446a18deb11
Mike Tsebo <32197449+TECT-2000@users.noreply.github.com>
2019-09-04 11:49:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    EMAIL = "projetcompte54@gmail.com"
-    MDP = "Dortmund7"
-    USERNAME="Antic"
+    EMAIL = "rantanplan@gmail.com"
+    MDP = "ronaldo"
+    USERNAME="Epiphane"
     MOT_DE_PASSE="1234"
